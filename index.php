715b2a62222974db1a06349a53ff6726eb16fb16
kushsharma <kushsharma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-18 09:50:48 +0000

    Css fix
    git-svn-id: https://plugins.svn.wordpress.org/kush-micro-news/trunk@950899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Kush Micro News
 Description: Spread the news in shortest possible way. Use links to refer data and title to concise it.
-Version: 1.4.2
+Version: 1.4.3
 Author: Kush Sharma
 Author Email: thekushsharma@gmail.com 
 Author URI: http://softnuke.com/
